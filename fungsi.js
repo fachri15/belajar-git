a81273616a000d16daf3e1d8088c9f6ed2de16c1
fachri15 <fachridatulotolomo1508@gmail.com>
2023-02-23 21:03:37 +0800

    Revisi Bahasa dan Perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1,2));
 const subtract = (a,b) => a - b;
 console.log(subtract(5,2));
 
-const multiplyNumber = (a,b) => a * b;
-console.log(multiplyNumber(2,2));
+const perkalian = (a,b) => a * b;
+console.log(perkalian(2,2));
 
 const divide = (a,b) => a / b;
 console.log(divide(4,2));
